af40fb4d89310ec511e0688f16861c0f3a0223b7
sulin <sisyphsu@gmail.com>
2020-12-05 12:44:07 +0800

    fix typo and remove useless files

--- emath/utils_calc.go
+++ emath/utils_calc.go
@@ -2,7 +2,7 @@ package emath
 
 func DivCeil(dividend, divisor int) (result int) {
 	if divisor == 0 {
-		panic("divison can't be 0")
+		panic("division can't be 0")
 	}
 	result = dividend / divisor
 	if dividend%divisor != 0 {
@@ -13,7 +13,7 @@ func DivCeil(dividend, divisor int) (result int) {
 
 func DivRound(dividend, divisor int) (result int) {
 	if divisor == 0 {
-		panic("divison can't be 0")
+		panic("division can't be 0")
 	}
 	result = dividend / divisor
 	if dividend%divisor >= divisor/2 {
